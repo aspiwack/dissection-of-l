a0f1551664fb11244b116d9e271cc21df1e39416
Arnaud Spiwack <arnaud@spiwack.net>
2014-01-16 10:49:13 +0000

    Further corrections of arrow directions for shifts.

--- util/prelude.ml
+++ util/prelude.ml
@@ -132,8 +132,8 @@ let pi1rule = index pi (mode M (text"1"))
 let pi2rule = index pi (mode M (text"2"))
 let bangrule = mode M (text"!")
 let whynotrule = mode M (text"?")
-let positiveshift = downarrow
-let negativeshift = uparrow
+let positiveshift = uparrow
+let negativeshift = downarrow
 
 let lambdap p = mode M (lambda^^p)
 let cutp p = mode M (cutrule^^text"\\,"^^p)
